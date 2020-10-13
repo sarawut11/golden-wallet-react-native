94e2de2a180b67204f0e013315cbc54923db61a9
sarawut <sanit.sa@outlook.com>
2020-10-13 23:57:31 +1000

    upgrade

--- ios/golden/AppDelegate.h
+++ ios/golden/AppDelegate.h
@@ -7,10 +7,8 @@
 
 #import <UIKit/UIKit.h>
 
-@import UserNotifications;
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate,UNUserNotificationCenterDelegate>
+@property (nonatomic, strong) UIWindow *window;
 
-  @property (nonatomic, strong) UIWindow *window;
-
-  @end
+@end
--- ios/golden/AppDelegate.m
+++ ios/golden/AppDelegate.m
@@ -9,11 +9,6 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import <Fabric/Fabric.h>
-#import <CoreSpotlight/CoreSpotlight.h>
-#import <Crashlytics/Crashlytics.h>
-#import "RNSplashScreen.h"
-#import "RNFIRMessaging.h"
 
 @implementation AppDelegate
 
@@ -34,58 +29,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  [Fabric with:@[[Crashlytics class]]];
-  [RNSplashScreen show];
-  [FIRApp configure];
-  [[UNUserNotificationCenter currentNotificationCenter] setDelegate:self];
-  [self spotlightSearch];
-  application.applicationIconBadgeNumber = 0;
   return YES;
 }
 
--(void)spotlightSearch{
-
-  CSSearchableItemAttributeSet *searchableItemAttributeSet = [[CSSearchableItemAttributeSet alloc] initWithItemContentType:@"io.goldenwallet"];
-  searchableItemAttributeSet.contentDescription = @"Golden";
-  searchableItemAttributeSet.title = @"Golden";
-  searchableItemAttributeSet.displayName = @"Golden";
-  searchableItemAttributeSet.keywords = @[@"wallet", @"ethereum", @"golden wallet ", @"bitcoin", @"golden", @"crypto"];
-  //    UIImage *thumbnail = [UIImage imageNamed:@"starwars_icon"];
-  //    searchableItemAttributeSet.thumbnailData = UIImageJPEGRepresentation(thumbnail, 0.7);
-
-  CSSearchableItem *searchableItem = [[CSSearchableItem alloc] initWithUniqueIdentifier:@"io.goldenwallet" domainIdentifier:@"io.goldenwallet" attributeSet:searchableItemAttributeSet];
-
-  CSSearchableIndex *defaultSearchableIndex = [CSSearchableIndex defaultSearchableIndex];
-  [defaultSearchableIndex indexSearchableItems:@[searchableItem] completionHandler:^(NSError * _Nullable error) {
-  }];
-}
-
-
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
-{
-  [RNFIRMessaging willPresentNotification:notification withCompletionHandler:completionHandler];
-}
-
-#if defined(__IPHONE_11_0)
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler
-{
-  UIApplication.sharedApplication.applicationIconBadgeNumber = 0;
-  [RNFIRMessaging didReceiveNotificationResponse:response withCompletionHandler:completionHandler];
-}
-#else
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)())completionHandler
-{
-  UIApplication.sharedApplication.applicationIconBadgeNumber = 0;
-  [RNFIRMessaging didReceiveNotificationResponse:response withCompletionHandler:completionHandler];
-}
-#endif
-
-//You can skip this method if you don't want to use local notification
--(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
-  [RNFIRMessaging didReceiveLocalNotification:notification];
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler{
-  [RNFIRMessaging didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
-}
 @end
